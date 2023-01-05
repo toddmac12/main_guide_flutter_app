51372fe41a6f8e4686204b8f6edf81fd7c8d1480
toddmac12 <toddmac12@users.noreply.github.com>
2023-01-05 01:55:13 -0500

    Update quiz.dart

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,24 +4,28 @@ import './answers.dart';
 
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
-  final int questionindex;
+  final int questionIndex;
   final Function answerQuestion;
 
-  Quiz(
-      {@required this.questions,
-      @required this.answerQuestion,
-      @required this.questionindex});
+  Quiz({
+    @required this.questions,
+    @required this.answerQuestion,
+    @required this.questionIndex,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Question(
-          questions[questionindex]['question'],
+          questions[questionIndex]['question'],
         ),
-        ...(questions[questionindex]['answer'] as List<Map<String, Object>>)
+        ...(questions[questionIndex]['answer'] as List<Map<String, Object>>)
             .map((answer) {
-          return Answer(() => answerQuestion(answer['score']), answer['text']);
+          return Answer(
+            () => answerQuestion(answer['score']),
+            answer['text'],
+          );
         }).toList()
       ],
     );
